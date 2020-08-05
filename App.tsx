6cdbf76333aeb4b8a900a5a040ee6ec08cdd4a3b
kvalenty <krushunin89@gmail.com>
2020-08-05 18:02:33 +0300

    add marker, drag map, change marker position

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {ScreenMap} from './src/components/Map';
 
 declare const global: {HermesInternal: null | {}};
@@ -7,7 +7,6 @@ declare const global: {HermesInternal: null | {}};
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Hello 12</Text>
       <ScreenMap />
     </View>
   );
@@ -16,9 +15,11 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#eee',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    width: '100%',
+    height: '100%',
   },
 });
 
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import React, {useState} from 'react';
+import MapView, {PROVIDER_GOOGLE, Marker, MapEvent} from 'react-native-maps';
 import {View, StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    // ...StyleSheet.absoluteFillObject,
-    height: 400,
-    width: 300,
-    flex: 1,
+    ...StyleSheet.absoluteFillObject,
+    // height: '50%',
+    // width: '10%',
+    // flex: 1,
     // justifyContent: 'flex-end',
     // alignItems: 'center',
   },
@@ -16,17 +16,55 @@ const styles = StyleSheet.create({
   },
 });
 
-export const ScreenMap = () => (
-  <View style={styles.container}>
-    <MapView
-      provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-      style={styles.map}
-      region={{
-        latitude: 50.45466,
-        longitude: 30.5238,
-        latitudeDelta: 0.05,
-        longitudeDelta: 0.05,
-      }}
-    />
-  </View>
-);
+interface Coordinates {
+  latitude: number;
+  longitude: number;
+}
+
+interface Region {
+  latitude: number;
+  longitude: number;
+  latitudeDelta: number;
+  longitudeDelta: number;
+}
+
+const initialCoordinates: Coordinates = {
+  latitude: 50.45466,
+  longitude: 30.5238,
+};
+
+const initialRegion: Region = {
+  latitude: 50.45466,
+  latitudeDelta: 0.05,
+  longitude: 30.5238,
+  longitudeDelta: 0.05,
+};
+
+export const ScreenMap = () => {
+  const [markerCoordinates, setMarkerCoordinates] = useState<Coordinates>(initialCoordinates);
+  const [currentCoordinates, setCurrentCoordinates] = useState<Region>(initialRegion);
+
+  const onPressGetCoordinates = ({nativeEvent}: MapEvent): void => setMarkerCoordinates(nativeEvent.coordinate);
+  const onPanDragGetCoordinates = (region: Region): void => setCurrentCoordinates(region);
+
+  const {latitude, longitude} = markerCoordinates;
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        onPress={onPressGetCoordinates}
+        onRegionChangeComplete={onPanDragGetCoordinates}
+        region={currentCoordinates}>
+        <Marker
+          coordinate={{
+            latitude,
+            longitude,
+          }}
+          icon={require('../images/marker-icon.png')}
+        />
+      </MapView>
+    </View>
+  );
+};
